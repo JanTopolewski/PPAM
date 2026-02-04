3fc87ab6737c10031b4ce7c93fef947fa5b18a58
JanTopolewski <jankotopolewski@gmail.com>
2026-02-04 12:14:55 +0100

    Add first part of the solutipon to the task from 4.02

---
+++ 2026_02_04/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment1 = new Fragment1();
+        fragmentTransaction.replace(R.id.main, fragment1);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        Button button =
+    }
+}
